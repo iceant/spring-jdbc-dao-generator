3a49b18927d5df74bee2b658579546ed3be64d0a
Chen Peng <pizer.chen@gmail.com>
2020-12-14 10:49:04 +0800

    cache 加上判断

--- spring-jdbc-dao-generator-webapp/src/main/resources/spring/jdbc/CacheConfigurationBean.java
+++ spring-jdbc-dao-generator-webapp/src/main/resources/spring/jdbc/CacheConfigurationBean.java
@@ -6,10 +6,12 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -17,16 +19,27 @@ import java.util.Arrays;
 @Configuration
 @EnableCaching
 public class CacheConfiguration extends CachingConfigurerSupport {
-    @Bean
-    public CacheManager cacheManager() {
+    @Value("${app.cache.enable}")
+    Boolean cacheEnable;
+
+    private CacheManager makeCacheManager(){
         SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
         simpleCacheManager.setCaches(Arrays.asList(
-            {{#tableBeans}}
-            new ConcurrentMapCache({{className}}.class.getSimpleName()),
-            {{/tableBeans}}
-            new ConcurrentMapCache("global")
+        {{#tableBeans}}
+        new ConcurrentMapCache({{className}}.class.getSimpleName()),
+        {{/tableBeans}}
+        new ConcurrentMapCache("global")
         ));
         return simpleCacheManager;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        if(cacheEnable){
+            return makeCacheManager();
+        }else{
+            return new NoOpCacheManager();
+        }
     }
 
     @Bean("keyGenerator")
